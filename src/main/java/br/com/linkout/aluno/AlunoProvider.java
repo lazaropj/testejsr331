828ddd5bbb3784cc3353e4aae6e781b2a32e49f0
Lazaro <linkout.lazaro@gmail.com>
2013-12-13 16:24:20 -0200

    Update AlunoProvider.java

--- src/main/java/br/com/linkout/aluno/AlunoProvider.java
+++ src/main/java/br/com/linkout/aluno/AlunoProvider.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.MediaType;
 import br.com.linkout.DAO.AlunoDAO;
 import br.com.linkout.modelo.Aluno;
 
+import org.glassfish.jersey.server.JSONP;
+
 @Path("/alunos")
 public class AlunoProvider {
 	
@@ -17,6 +19,7 @@ public class AlunoProvider {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@JSONP(callback = "testeLazaro", queryParam = "jsonpCallback")
 	public List<Aluno> getAll() {
 		dao.beginTransaction();
 		List<Aluno> listaDeAlunos = dao.findAll();
